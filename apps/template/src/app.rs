fef1457da48999965463fec5883b04836da9f657
Matthew J. Berger <matthewjordanberger@gmail.com>
2024-04-29 01:01:51 -0600

    refactor

---
+++ apps/template/src/app.rs
@@ -0,0 +1,23 @@
+use nightmare::prelude::*;
+
+#[derive(Default)]
+pub struct App;
+
+impl State for App {
+    fn title(&self) -> &str {
+        "Nightmare"
+    }
+
+    fn initialize(&mut self, _context: &mut Context) {}
+
+    fn receive_event(&mut self, _context: &mut Context, _event: &winit::event::Event<()>) {}
+
+    fn update(&mut self, _context: &mut Context, ui: &egui::Context) {
+        egui::Window::new("Game").show(ui, |ui| {
+            ui.heading("Hello, world!");
+            if ui.button("Click me!").clicked() {
+                log::info!("Button clicked!");
+            }
+        });
+    }
+}
